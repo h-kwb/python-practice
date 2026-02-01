bd83522bdd05e1a0f75107e3c0186678fae00527
h-kwb <k.hiroto.6302@gmail.com>
2026-02-01 17:42:08 +0900

    Update if_else.py
    例外処理を実装

--- basic/if_else.py
+++ basic/if_else.py
@@ -1,8 +1,11 @@
-number = int(input("数を入力してください。\n正負を判定します。"))
+try:
+  number = int(input("数を入力してください。\n正負を判定します。"))
 
-if number > 0:
-  print(f"{number}は、正の数です。")
-elif number == 0:
-  print(f"{number}は、0です。")
-else:
-  print(f"{number}は、負の数です。")
+  if number > 0:
+    print(f"{number}は、正の数です。")
+  elif number == 0:
+    print(f"{number}は、0です。")
+  else:
+    print(f"{number}は、負の数です。")
+except ValueError:
+  print("数字を入力してください。")
